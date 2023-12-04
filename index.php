b3bb8483a8516332907962fc2faa80c2151fa8f9
Andrianina24 <rakotozananya5@gmail.com>
2023-12-04 06:32:02 +0300

    update

--- index.php
+++ index.php
@@ -12,11 +12,6 @@ Flight::before('start', function(&$params, &$output){
 
 Flight::route('/',function () {
 
-    // header('Access-Control-Allow-Origin: *');
-    // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    // header('Access-Control-Allow-Headers: Content-Type');
-    // header('Content-Type: application/json');
-
     $pdo = Connexion::connect();
     $query = $pdo->query('SELECT * FROM statsEquipeGeneralGeneral');
 
@@ -26,11 +21,6 @@ Flight::route('/',function () {
 );
 
 Flight::route('/general',function () {
-
-        // header('Access-Control-Allow-Origin: *');
-        // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        // header('Access-Control-Allow-Headers: Content-Type');
-        // header('Content-Type: application/json');
 
         $pdo = Connexion::connect();
         $query = $pdo->query('SELECT * FROM statsEquipeGeneralGeneral');
