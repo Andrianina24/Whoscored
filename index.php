c22a212425bfc8d42f2c580cc8035e8181dfbdfd
Andrianina24 <rakotozananya5@gmail.com>
2023-12-04 06:25:11 +0300

    index

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require 'flight/Flight.php';
 
 Flight::before('start', function(&$params, &$output){
     header('Access-Control-Allow-Origin: *');
-    // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    // header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
     // header('Content-Type: application/json');
 });
 
